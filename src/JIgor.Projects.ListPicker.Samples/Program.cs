aa9a96672e295cba1dc1f49f2e40e9e459d228b7
Jose Igor <j.igor@outlook.com>
2021-11-30 21:58:42 -0300

    Formating samples project

--- src/JIgor.Projects.ListPicker.Samples/Program.cs
+++ src/JIgor.Projects.ListPicker.Samples/Program.cs
@@ -7,7 +7,7 @@ namespace JIgor.Projects.ListPicker.Samples
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Sample1()
         {
             var picker = new ListPicker();
 
@@ -24,18 +24,40 @@ namespace JIgor.Projects.ListPicker.Samples
                     new User(8, "Manning"),
                 };
 
-                // var removedValues = picker.Pick(myList, 1);
-
-                var removedValues = picker.PickElements(myList, 6, p => (p.Id == 5 || p.Name == "Favre" || p.Name == "Manning"));
-
-                // var newList = myList.RemoveRandomly(4, p => p.Id == 2);
-
-                // newList.ToList().ForEach(p => Console.WriteLine(p.ToString()));
+                var removedValues = picker.PickElements(myList, 6,
+                    p => (p.Id == 5 || p.Name == "Favre" || p.Name == "Manning"));
                 removedValues.ToList().ForEach(p => Console.WriteLine(p.ToString()));
-
-                // Console.WriteLine(newList.Count());
                 Console.WriteLine("-----------------");
             }
+        }
+
+        public static void Sample2()
+        {
+            var picker = new ListPicker();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var myList = new List<User>()
+                {
+                    new User(1, "Manning"),
+                    new User(2, "Igor"),
+                    new User(4, "Igor"),
+                    new User(5, "Brady"),
+                    new User(6, "Manning"),
+                    new User(7, "Favre"),
+                    new User(8, "Manning"),
+                };
+
+                var removedValues = picker.PickElements(myList, 6);
+                removedValues.ToList().ForEach(p => Console.WriteLine(p.ToString()));
+                Console.WriteLine("-----------------");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Sample1();
+            Sample2();
         }
     }
 }
